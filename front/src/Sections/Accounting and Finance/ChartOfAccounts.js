4b1fb3a7e4a5e8a597003cbf1ac136467e044de6
vusalGaragozov <vusalgaragozov@gmail.com>
2023-08-24 17:04:57 +0400

    24 aug changes

--- front/src/Sections/Accounting and Finance/ChartOfAccounts.js
+++ front/src/Sections/Accounting and Finance/ChartOfAccounts.js
@@ -1,27 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import './custom-styles.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMinus } from '@fortawesome/free-solid-svg-icons';
 import * as XLSX from 'xlsx'; // Use the * as syntax
+import { AuthContext } from '../AuthContext.js'; //
 
 
 const columnMapping = {
     'Report': 'report',
-    'Class': 'class',
+    'Class': 'accountClass',
     'Caption': 'caption',
     'FS Line': 'fsLine',
     'Currency': 'currency'
   };
 
 function ChartOfAccounts() {
+    const { user } = useContext(AuthContext);
+const loggedInUserId = user._id;
   const [accountData, setAccountData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [excelFile, setExcelFile] = useState(null); 
 
   const [newAccount, setNewAccount] = useState({
     report: '',
-    class: '',
+    accountClass: '',
     caption: '',
     fsLine: '',
     currency: ''
@@ -31,7 +34,8 @@ function ChartOfAccounts() {
     async function fetchData() {
       try {
         const response = await axios.get('/api/accounts');
-        setAccountData(response.data);
+        const userAccounts = response.data.filter(account => account.userId === loggedInUserId);
+        setAccountData(userAccounts);
       } catch (error) {
         console.error('Error fetching accounts:', error);
       } finally {
@@ -39,7 +43,8 @@ function ChartOfAccounts() {
       }
     }
     fetchData();
-  }, []);
+  }, [loggedInUserId]);
+  
 
   const handleDeleteAccount = async (accountId) => {
     try {
@@ -63,7 +68,7 @@ function ChartOfAccounts() {
   const handleAddAccount = async () => {
     if (
       !newAccount.report ||
-      !newAccount.class ||
+      !newAccount.accountClass ||
       !newAccount.caption ||
       !newAccount.fsLine ||
       !newAccount.currency
@@ -77,7 +82,7 @@ function ChartOfAccounts() {
         setAccountData((prevData) => [...prevData, newAccount]);
         setNewAccount({
           report: '',
-          class: '',
+          accountClass: '',
           caption: '',
           fsLine: '',
           currency: ''
@@ -97,24 +102,49 @@ function ChartOfAccounts() {
   };
 
   const handleFileUpload = async () => {
-   if (!excelFile) { 
-    console.log('No file selected')
+    if (!excelFile) {
+      console.log('No file selected');
       return;
     }
+    console.log('Uploading file:', excelFile);
+    const formData = new FormData();
+    formData.append('file', excelFile);
+  
+    console.log('Request payload:', formData); // Log the FormData object
 
+  
     const reader = new FileReader();
     reader.onload = async (e) => {
+      console.log('File read successfully'); // Add this console log
+  
       const data = new Uint8Array(e.target.result);
+      console.log('Raw data:', data); // Add this console log
+  
       const workbook = XLSX.read(data, { type: 'array' });
+      console.log('Workbook:', workbook); // Add this console log
+  
       const sheetName = workbook.SheetNames[0];
+      console.log('Sheet name:', sheetName); // Add this console log
+  
       const worksheet = workbook.Sheets[sheetName];
+      console.log('Worksheet:', worksheet); // Add this console log
+  
       const range = XLSX.utils.decode_range(worksheet['!ref']);
-      const maxRow = Math.min(300, range.e.r); // Limit to the first 300 rows
-      const maxCol = Math.min(49, range.e.c); // Limit to the first 50 columns
-
-      const parsedData = XLSX.utils.sheet_to_json(worksheet, { range: { s: { r: 0, c: 0 }, e: { r: maxRow, c: maxCol } }, header: 1 });
-
+      console.log('Range:', range); // Add this console log
+  
+      const maxRow = Math.min(300, range.e.r);
+      const maxCol = Math.min(49, range.e.c);
+  
+      const parsedData = XLSX.utils.sheet_to_json(worksheet, {
+        range: { s: { r: 0, c: 0 }, e: { r: maxRow, c: maxCol } },
+        header: 1
+      });
+      console.log('Parsed data:', parsedData); // Add this console log
+  
       const [header, ...rows] = parsedData;
+      console.log('Header:', header); // Add this console log
+      console.log('Rows:', rows); // Add this console log
+  
       const accountsToAdd = rows.map((row) => {
         const account = {};
         for (let i = 0; i < header.length; i++) {
@@ -125,18 +155,27 @@ function ChartOfAccounts() {
         }
         return account;
       });
-
+  
+      console.log('Accounts to add:', accountsToAdd); // Add this console log
+  
       try {
+
         const response = await axios.post('/api/accounts/batch', accountsToAdd);
         if (response.status === 201) {
           setAccountData((prevData) => [...prevData, ...accountsToAdd]);
         }
       } catch (error) {
         console.error('Error uploading accounts:', error);
+        if (error.response) {
+            console.log('Error response data:', error.response.data);
+            console.log('Error response status:', error.response.status);
+            console.log('Error response headers:', error.response.headers);
+          }
       }
     };
     reader.readAsArrayBuffer(excelFile);
   };
+  
 
   return (
     <div className="container mt-5">
@@ -170,7 +209,7 @@ function ChartOfAccounts() {
                 }}
               >
                 <td>{account.report}</td>
-                <td>{account.class}</td>
+                <td>{account.accountClass}</td>
                 <td>{account.caption}</td>
                 <td>{account.fsLine}</td>
                 <td>{account.currency}</td>
@@ -216,8 +255,8 @@ function ChartOfAccounts() {
               <label>Class</label>
               <input
                 type="text"
-                name="class"
-                value={newAccount.class}
+                name="accountClass"
+                value={newAccount.accountClass}
                 onChange={handleInputChange}
                 className="form-control"
               />
